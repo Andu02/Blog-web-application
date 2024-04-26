ba11649fc556c650857a7896d37ccc16c4e191a9
Scoicaru Andrei Alexandru <andreiscoicaru2002@gmail.com>
2024-04-26 21:19:18 +0300

    some improvements

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js
-
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -22,187 +20,197 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let postsArray = [];
 
 // Set EJS as view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // HOME PAGE
-app.get('/', (req, res) => {
-    res.render('index', { posts: postsArray });
+app.get("/", (req, res) => {
+  res.render("index", { posts: postsArray });
 });
 
 // Create new post
-app.get('/create', (req, res) => {
-    res.render('createPost', {posts : postsArray});
+app.get("/create", (req, res) => {
+  res.render("createPost", { posts: postsArray });
 });
 
 // Post new post
-app.post('/create', validatePost(true, false, 'createPost'), (req, res) => {
-    let { title, content } = req.body;
+app.post("/create", validatePost(true, false, "createPost"), (req, res) => {
+  let { title, content } = req.body;
 
-    const newPost = {
-        id: generateId(), 
-        date: new Date().toDateString(),
-        hour: new Date().getHours() + ":" + new Date().getMinutes(),
-        author: "unknown",
-        comments: [],
-        numberComments: 0,
-        isLiked: false,
-        title,
-        isEdited: "",
-        content,
-        author: "Unknown"
-    };
-    postsArray.push(newPost);
-    res.redirect('/');
+  const newPost = {
+    id: generateId(),
+    date: new Date().toDateString(),
+    hour: new Date().getHours() + ":" + String(new Date().getMinutes()).padStart(2,'0'),
+    author: "unknown",
+    comments: [],
+    numberComments: 0,
+    isLiked: false,
+    title,
+    isEdited: "",
+    content,
+    author: "Unknown",
+  };
+  postsArray.push(newPost);
+  res.redirect("/");
 });
 
 // Get post
-app.get('/post/:id', (req, res) => {
-    const postId = req.params.id;
-    const post = postsArray.find(post => post.id === postId);
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-    res.render('seePost', { post, posts: postsArray }); 
+app.get("/post/:id", (req, res) => {
+  const postId = req.params.id;
+  const post = postsArray.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  res.render("seePost", { post, posts: postsArray });
 });
 
 // Post delete
-app.post('/post/delete/:id', (req, res) => {
-    const postId = req.params.id;
-    postsArray = postsArray.filter(post => post.id !== postId);
-    res.redirect('/');
+app.post("/post/delete/:id", (req, res) => {
+  const postId = req.params.id;
+  postsArray = postsArray.filter((post) => post.id !== postId);
+  res.redirect("/");
 });
 
-// Create comment 
-app.get('/post/comment/:id', (req, res) => {
-    const postId = req.params.id;
-    const post = postsArray.find(post => post.id === postId);
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-    res.render('createComment', { post, posts: postsArray }); 
+// Create comment
+app.get("/post/comment/:id", (req, res) => {
+  const postId = req.params.id;
+  const post = postsArray.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  res.render("createComment", { post, posts: postsArray });
 });
 
 // Post new comment
-app.post('/create-comment/:id', validatePost(false, true, 'createComment'), (req, res) => {
-    const postId = req.params.id; 
-    const post = postsArray.find(post => post.id === postId);
+app.post(
+  "/create-comment/:id",
+  validatePost(false, true, "createComment"),
+  (req, res) => {
+    const postId = req.params.id;
+    const post = postsArray.find((post) => post.id === postId);
     const { content } = req.body;
 
     if (post) {
-        const comment = {
-            content: content,
-            date: new Date().toDateString(),
-            hour: new Date().getHours() + ":" + new Date().getMinutes(),
-            author: "Unknown"
-        };
-        post.comments.push(comment);
-        post.numberComments++;
-        res.redirect('/post/' + postId);
+      const comment = {
+        content: content,
+        date: new Date().toDateString(),
+        hour: new Date().getHours() + ":" + String(new Date().getMinutes()).padStart(2,'0'),
+        author: "Unknown",
+      };
+      post.comments.push(comment);
+      post.numberComments++;
+      res.redirect("/post/" + postId);
     } else {
-        res.status(404).send("Post not found");
+      res.status(404).send("Post not found");
     }
-});
+  }
+);
 
 // Redirect previous page
-app.post('/redirect', (req, res) => {
-    const returnTo = req.body.returnTo || '/';
-    res.redirect(returnTo);
+app.post("/redirect", (req, res) => {
+  const returnTo = req.body.returnTo || "/";
+  res.redirect(returnTo);
 });
 
 // Create new edit
-app.get('/post/edit/:id', (req, res) => {
-    const postId = req.params.id;
-    const post = postsArray.find(post => post.id === postId);
-    if (!post) {
-        res.sendStatus(404);
-        return;
-    }
-    res.render('editPost', { post, posts: postsArray });
+app.get("/post/edit/:id", (req, res) => {
+  const postId = req.params.id;
+  const post = postsArray.find((post) => post.id === postId);
+  if (!post) {
+    res.sendStatus(404);
+    return;
+  }
+  res.render("editPost", { post, posts: postsArray });
 });
 
 // Post edit
-app.post('/post/edit/:id', validatePost(true, true, 'editPost'), (req, res) => {
-    const postId = req.params.id;
-    const { title, content } = req.body;
-    const postIndex = postsArray.findIndex(post => post.id === postId);
-    if (postIndex === -1) {
-        res.sendStatus(404);
-        return;
-    }
-    postsArray[postIndex] = { ...postsArray[postIndex], title, content };
-    postsArray[postIndex].isEdited = "(EDITED)";
-    res.redirect('/post/' + postId);
+app.post("/post/edit/:id", validatePost(true, true, "editPost"), (req, res) => {
+  const postId = req.params.id;
+  const { title, content } = req.body;
+  const postIndex = postsArray.findIndex((post) => post.id === postId);
+  if (postIndex === -1) {
+    res.sendStatus(404);
+    return;
+  }
+  postsArray[postIndex] = { ...postsArray[postIndex], title, content };
+  postsArray[postIndex].isEdited = "(EDITED)";
+  res.redirect("/post/" + postId);
 });
 
 // Add like to post
-app.post('/post/like/:id', (req, res) => {
-    const postId = req.params.id;
-    const post = postsArray.find(post => post.id === postId);
-    if (!post) {
-      return res.status(404).send("Post not found");
-    }
-    post.isLiked = !post.isLiked;
-    res.redirect('/');
-  });
-  
+app.post("/post/like/:id", (req, res) => {
+  const postId = req.params.id;
+  const post = postsArray.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  post.isLiked = !post.isLiked;
+  res.redirect("/");
+});
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // Function to generate unique IDs
 function generateId() {
-    return Math.random().toString(36).substring(2, 9);
+  return Math.random().toString(36).substring(2, 9);
+}
+
+function containsOnlyWhitespace(str) {
+  return str.trim().length === 0;
 }
 
 // To restrict the character limit
 function validatePost(requireTitle, requirePost, view) {
-    return function(req, res, next) {
+  return function (req, res, next) {
+    let title = "";
+    let content = "";
+    const errors = [];
 
-        let title = "";
-        let content = "";
+    if (requireTitle) {
+      ({ title, content } = req.body);
+    } else {
+      ({ content } = req.body);
+    }
 
-        if(requireTitle) {
-            ({ title, content } = req.body);
-        }
-        else {
-            ({ content } = req.body);
-        }
+    if (requireTitle && containsOnlyWhitespace(title)) {
+      errors.push('Title cannot be empty or contain only whitespace.');
+    }
 
-        const errors = [];
+    // Check if content is required and validate
+    if (containsOnlyWhitespace(content)) {
+      errors.push('Content cannot be empty or contain only whitespace.');
+    }
 
-        let post = "";
-        if(requirePost) {
-            const postId = req.params.id;
-            post = postsArray.find(post => post.id === postId);
-        }
+    let post = "";
+    if (requirePost) {
+      const postId = req.params.id;
+      post = postsArray.find((post) => post.id === postId);
+    }
 
-        if (requireTitle && (!title || title.length < minTitleLength || title.length > maxTitleLength)) {
-            errors.push(`The title should have a minimum of ${minTitleLength} characters and a maximum of ${maxTitleLength}.`);
-        }
+    if (requireTitle && (!title || title.length < minTitleLength || title.length > maxTitleLength)) {
+      errors.push(`The title should have a minimum of ${minTitleLength} characters and a maximum of ${maxTitleLength}.`);
+    }
 
-        if (!content || content.length < minContentLength || content.length > maxContentLength) {
-            errors.push(`The content should have a minimum of ${minContentLength} characters and a maximum of ${maxContentLength}.`);
-        }
+    if (!content || content.length < minContentLength || content.length > maxContentLength) {
+      errors.push(`The content should have a minimum of ${minContentLength} characters and a maximum of ${maxContentLength}.`);
+    }
 
-        if (errors.length > 0) {
-            // There are validation errors, render the form again with error messages
-            res.render(view, {
-                errors,
-                post: post,
-                title: requireTitle ? (title ? title.substring(0, maxTitleLength) : "") : "", // Truncate title if it's too long
-                content: content ? content.substring(0, maxContentLength) : "" // Truncate content if it's too long
-            });
-        } else {
-            // No validation errors, proceed to the next middleware or route handler
-            next();
-        }
-    };
+    if (errors.length > 0) {
+      // There are validation errors, render the form again with error messages
+      res.render(view, {
+        errors,
+        post: post,
+        title: requireTitle ? (title ? title.substring(0, maxTitleLength) : "") : "", // Truncate title if it's too long
+        content: content ? content.substring(0, maxContentLength) : "", // Truncate content if it's too long
+      });
+    } else {
+      // No validation errors, proceed to the next middleware or route handler
+      next();
+    }
+  };
 }
-
-
-
--- public/scripts/likePost.js
+++ public/scripts/likePost.js
@@ -1,32 +1,21 @@
 function likePost(postId) {
-    fetch(`/post/like/${postId}`, {
-        method: 'POST'
-})
-.then(response => {
-    if (!response.ok) {
-    throw new Error('Failed to like post');
-    }
-    const likeImage = document.getElementById(`likeImage_${postId}`);
-    if (likeImage) {
+  fetch(`/post/like/${postId}`, {
+    method: "POST",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to like post");
+      }
+      const likeImage = document.getElementById(`likeImage_${postId}`);
+      if (likeImage) {
         if (likeImage.src.includes("/icons/like-unpressed.png")) {
-            likeImage.src = "/icons/like-pressed.png"; 
+          likeImage.src = "/icons/like-pressed.png";
         } else {
-            likeImage.src = "/icons/like-unpressed.png"; 
+          likeImage.src = "/icons/like-unpressed.png";
         }
-    }
-})
-.catch(error => {
-    console.error(error);
-});
-}
-
-var myButtons = document.querySelectorAll('.w3-button');
-
-myButtons.forEach(function(button) {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        console.log('Button clicked:', button.textContent);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     });
-});
-
+}
